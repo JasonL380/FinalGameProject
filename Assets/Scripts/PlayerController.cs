7fed6e4005ecd2ffd8d7689359de339084ffcb59
Jason Leech <jason.leech@digipen.edu>
2023-05-04 12:14:02 -0700

    Player controller with jumping, not completely functional

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,82 @@
+using System;
+using UnityEditor.Experimental.GraphView;
+using UnityEngine;
+
+namespace DefaultNamespace
+{
+    [RequireComponent(typeof(Collider2D), typeof(Rigidbody2D))]
+    public class PlayerController : MonoBehaviour
+    {
+        public float moveSpeed;
+        public float jumpSpeed;
+        public float gravity = -9.81f;
+        
+        public Vector3 pos3d;
+        public Vector3 velocity3d = Vector3.zero;
+        private Rigidbody2D myRB2D;
+        public bool onGround = true;
+        private Collider2D myCol2D;
+        public GameObject LandingTarget;
+        private Collider2D targetCol2D;
+
+        private SpriteRenderer mySpriteRenderer;
+        
+        
+        private void Start()
+        {
+            //print(gameObject.layer);
+            pos3d = transform.position;
+            myRB2D = GetComponent<Rigidbody2D>();
+            myCol2D = GetComponent<Collider2D>();
+            targetCol2D = LandingTarget.GetComponent<Collider2D>();
+            mySpriteRenderer = GetComponent<SpriteRenderer>();
+        }
+
+        private void Update()
+        {
+            //fake 3d physics
+            if (!onGround)
+            {
+                if (myCol2D.IsTouching(targetCol2D))
+                {
+                    onGround = true;
+                    gameObject.layer = 7;
+                    mySpriteRenderer.rendererPriority = 1;
+                    //targetCol2D.isTrigger = true;
+                    velocity3d.z = 0;
+                    transform.position = new Vector3(transform.position.x, pos3d.y);
+                }
+                else
+                {
+                    velocity3d.z += gravity * Time.deltaTime;
+                }
+            }
+            else
+            {
+                if (Input.GetButton("Jump"))
+                {
+                    onGround = false; 
+                    
+                    mySpriteRenderer.rendererPriority = 2;
+                    gameObject.layer = 8;
+                    //targetCol2D.isTrigger = false;
+                    velocity3d.z = jumpSpeed;
+
+                    pos3d.z = 0;
+                }
+
+                velocity3d.x = Input.GetAxis("Horizontal") * moveSpeed;
+
+                velocity3d.y = Input.GetAxis("Vertical") * moveSpeed;
+
+            }
+            
+            pos3d.x = transform.position.x;
+            pos3d += velocity3d * Time.deltaTime;
+
+            LandingTarget.transform.position = new Vector2(pos3d.x, pos3d.y);
+            
+            myRB2D.velocity = new Vector2(velocity3d.x, velocity3d.y + velocity3d.z);
+        }
+    }
+}
