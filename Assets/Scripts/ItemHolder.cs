cdbbee462b61ea32c90402dda732a11debbbfbd4
Jason Leech <jason.leech@digipen.edu>
2023-05-30 13:54:57 -0700

    Up right item hand sync

---
+++ Assets/Scripts/ItemHolder.cs
@@ -0,0 +1,13 @@
+// Name: Jason Leech
+// Date: 05/30/2023
+// Desc:
+
+using UnityEngine;
+
+namespace DefaultNamespace
+{
+    public class ItemHolder : MonoBehaviour
+    {
+        public int order;
+    }
+}
---
+++ Assets/Scripts/ItemRenderOrder.cs
@@ -0,0 +1,43 @@
+// Name: Jason Leech
+// Date: 05/30/2023
+// Desc:
+
+using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace DefaultNamespace
+{
+    [ExecuteInEditMode]
+    public class ItemRenderOrder : MonoBehaviour
+    {
+        public SpriteRenderer spriteRenderer;
+
+        public ParticleSystemRenderer particleSystem;
+
+        public ItemHolder holder;
+
+        public bool hasHolder = false;
+        
+        private void Start()
+        {
+            spriteRenderer = GetComponent<SpriteRenderer>();
+            particleSystem = GetComponent<ParticleSystemRenderer>();
+            holder = GetComponentInParent<ItemHolder>();
+
+            if (holder != null)
+            {
+                hasHolder = true;
+            }
+        }
+
+        private void Update()
+        {
+            if (hasHolder)
+            {
+                spriteRenderer.sortingOrder = holder.order;
+                particleSystem.sortingOrder = holder.order + 1;
+            }
+        }
+    }
+}
