731516a376b0f88b06522429833d942bc1020164
elementalearth123 <72054738+elementalearth123@users.noreply.github.com>
2023-06-02 13:12:21 -0700

    candle ui working

--- Assets/Scripts/ImageChange.cs
+++ Assets/Scripts/ImageChange.cs
@@ -9,56 +9,28 @@ using UnityEngine.UI;
 public class ImageChange : MonoBehaviour
 {
     public Image image;
-    public ParticleSystem pr;
-    public Sprite[] images;
 
-    private ParticleSystem.ShapeModule ps;
+    //public ParticleSystem pr;
+    //private ParticleSystem.ShapeModule ps;
 
     public float[] decrease;
-    public float tick;
-    public float currentTime;
 
-    public Vector3 initLightPos;
+    //public Vector3 initLightPos;
 
     //0 = candle, 1 = flashlight
-    public int typeLight;
 
-    public int currentImage = 0;
-
-    public GameObject player;
+    public GameObject candle;
     private void Start()
     {
         image = gameObject.GetComponent<Image>();
-        pr = gameObject.GetComponent<ParticleSystem>();
-        ps = pr.shape;
-        initLightPos = pr.shape.position;
-        currentTime = tick;
+        //pr = gameObject.GetComponent<ParticleSystem>();
+        //ps = pr.shape;
+        //initLightPos = pr.shape.position;
+        
     }
     // Update is called once per frame
     void Update()
     {
-        if (currentTime < 0 && currentImage < images.Length - 1)
-        {
-            ++currentImage;
-            image.sprite = images[currentImage];
-            currentTime = tick;
-            Vector3 py = ps.position;
-            py.y -= decrease[currentImage];
-            ps.position = py;
-        }
-        else if (currentTime < 0 && currentImage >= images.Length - 1 && player.GetComponent<PlayerController>().numLights[typeLight] > 0)
-        {
-            currentImage = 0;
-            currentTime = tick;
-            image.sprite = images[currentImage];
-            --player.GetComponent<PlayerController>().numLights[typeLight];
-            ps.position = initLightPos;
-        }
-        else if (currentTime < 0 && currentImage >= images.Length - 1 && player.GetComponent<PlayerController>().numLights[typeLight] <= 0)
-        {
-            //GetComponent<Light2D>().enabled = false;
-            GetComponent<ParticleSystemRenderer>().enabled = false;
-        }
-        currentTime -= Time.deltaTime;
+        image.sprite = candle.GetComponent<SpriteRenderer>().sprite;
     }
 }
