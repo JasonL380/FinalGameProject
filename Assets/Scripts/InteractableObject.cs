631ad53c6c56b14bd5a1e353f8f3b3d032fdfea3
elementalearth123 <72054738+elementalearth123@users.noreply.github.com>
2023-06-09 14:14:35 -0700

    key should work

--- Assets/Scripts/InteractableObject.cs
+++ Assets/Scripts/InteractableObject.cs
@@ -13,28 +13,51 @@ public class InteractableObject : MonoBehaviour
 
     RoomList roomList;
 
+    public bool playerNear;
+    public Collider2D player;
+
     // Start is called before the first frame update
     void Start()
     {
         gameObject.GetComponent<Collider2D>().isTrigger = true;
-        randLight = Random.Range(0, 51);
+        randLight = Random.Range(0, 52);
         beenRansacked = false;
         roomList = GetComponentInParent<RoomList>();
     }
 
-    // Update is called once per frame
-    private void OnTriggerStay2D(Collider2D collision)
+    private void OnTriggerEnter2D(Collider2D collision)
     {
-        if (Input.GetKeyDown(KeyCode.E) && collision.name.Equals("Player") && !beenRansacked)
+        if(collision.name.Equals("Player"))
         {
-            if(roomList.hasKey == false && roomList.numInteractableObjects - roomList.numRansackedObjects == 1)
+            playerNear = true;
+            player = collision;
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.name.Equals("Player"))
+        {
+            playerNear = false;
+            player = collision;
+        }
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.E) && playerNear && !beenRansacked)
+        {
+            //print("E " + randLight);
+            if (roomList.hasKey == false && roomList.numInteractableObjects - roomList.numRansackedObjects == 1)
             {
+                //print("Shouldn't show");
                 roomList.hasKey = true;
             }
             else
             {
                 if (randLight == 51 && roomList.hasKey != true)
                 {
+                    //print("key should be given");
                     //give key
                     roomList.hasKey = true;
                     roomList.numRansackedObjects++;
@@ -42,15 +65,16 @@ public class InteractableObject : MonoBehaviour
                 }
                 else
                 {
+                    //print("Light should be given");
                     if (randLight % 2 == 0)
                     {
                         //give candle
-                        ++collision.GetComponent<PlayerController>().numLights[0];
+                        ++player.GetComponent<PlayerController>().numLights[0];
                     }
                     else if (randLight % 2 == 1)
                     {
                         //give battery
-                        ++collision.GetComponent<PlayerController>().numLights[1];
+                        ++player.GetComponent<PlayerController>().numLights[1];
                     }
                     roomList.numRansackedObjects++;
                 }
@@ -58,5 +82,5 @@ public class InteractableObject : MonoBehaviour
             beenRansacked = true;
         }
     }
-    
+
 }
